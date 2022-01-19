b1d203e621416c0db9fdf0484ebd45185fda8768
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-18 23:59:43 -0500

    Refactoring our Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
+
 
 export default function Button(props) {
-   let buttonClass = "button";
+   // let buttonClass = "button";
 
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   }
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   // if (props.confirm) {
+   //    buttonClass += " button--confirm";
+   // }
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   // }
+
+   let buttonClass = classNames('button', {
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   });
 
 
    return (
