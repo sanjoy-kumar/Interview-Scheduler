8947b47901a75cc6a6673e685d99454e74fff985
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-20 12:15:19 -0500

    More Appointment Views added.

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'components/Appointment/styles.scss'
+import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
+
+
+
+
+export default function Appointment(props)  {
+
+  return (
+    <article className="appointment">
+      
+
+
+    </article>
+  );
+
+
+
+}
