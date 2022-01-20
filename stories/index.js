3c8890a6782e9abc5fc49657d190475a6765eed8
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-20 10:13:02 -0500

    Controlled Lists

--- stories/index.js
+++ stories/index.js
@@ -135,13 +135,13 @@ storiesOf("InterviewerList", module)
   .add("Selected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
+      value={3}
     />
   ))
   .add("Clickable", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("setInterviewer")}
     />
   ));
 
