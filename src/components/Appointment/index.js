835a43820a0bdfcdef4753eeb820c9156e810d12
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-20 17:41:29 -0500

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import 'components/Appointment/styles.scss'
+
 import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
+import Status from 'components/Appointment/Status';
+import Confirm from 'components/Appointment/Confirm';
+import Error from 'components/Appointment/Error';
 
 
 
 
-export default function Appointment(props)  {
+
+export default function Appointment(props) {
+  
+  const result = props.interview ? <Show />:<Empty />
+
 
   return (
     <article className="appointment">
-      
+
+      <Header time={props.time} />
+       {result}
+
+
+
+
 
 
     </article>
