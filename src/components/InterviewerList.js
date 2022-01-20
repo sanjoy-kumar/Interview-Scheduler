ad861d2cec2501d99d18289b511ebe3c2ee7a564
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-19 23:22:52 -0500

    Built the interview list.

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'components/InterviewerList.scss';
+import InterviewerListItem from 'components/InterviewerListItem';
+
+
+export default function InterviewerList(props) {
+
+  const interviewers = props.interviewers.map(interviewerObj => {
+
+    return (
+      <InterviewerListItem 
+        key={interviewerObj.id}
+        name={interviewerObj.name}
+        avatar={interviewerObj.avatar}
+        selected = {interviewerObj.id === props.interviewer}
+        setInterviewer = {props.setInterviewer} 
+      />
+    )
+  })
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewers</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section> 
+  )
+}
