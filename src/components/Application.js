29415fc64d9c071387a60a397ea9b87266a47cbc
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-23 11:20:21 -0500

    Managing State

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,14 +6,32 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import InterviewerList from "./InterviewerList";
 import Appointment from "components/Appointment";
-import useApplicationData from "hooks/useApplicationData";
-
+//import useApplicationData from "hooks/useApplicationData";
+import axios from "axios";
 
 
 export default function Application(props) {
 
-  const { state, setDay } = useApplicationData();
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
+
+  
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));   
+}
+
+  useEffect(() => {
+    axios.get("/api/days").then(response => setDays(response.data));
+
+    
+  }, []);
+
+  
 
   return (
     <main className="layout">
