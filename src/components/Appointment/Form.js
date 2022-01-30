9191e3ac25a6ce4160730e55c8be21881ab82031
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-30 14:16:08 -0500

    Update Appointment's Form.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,29 +6,28 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
 
   //For keeping track of the name
-  const [student, setStudent] = useState(props.name || "");
-  const [interviewer, setInterviewer] = useState(props.value || null)
+  const [currentName, setName] = useState(props.name || "");
+  const [currentInterviewer, setInterviewer] = useState(props.value || null)
   const [error, setError] = useState("");
 
   //Helper function to clear all fields
   const reset = () => {
-    setStudent("")
+    setName("")
     setInterviewer(null)
   }
 
   function cancel () {
-    // props.onCancel;
     reset();
     props.onCancel()
   }
 
   function validate() {
-    if (student === "") {
+    if (currentName === "") {
       setError("Student name cannot be blank")
       return;
     }
     setError("")
-    props.onSave(student, interviewer);
+    props.onSave(currentName, currentInterviewer);
   }
 
   return (
@@ -39,14 +38,15 @@ export default function Form(props) {
             className="appointment__create-input text--semi-bold"
             name={props.name}
             type="text"        
-            onChange={(event) => setStudent(event.target.value)}            
-            value={student}
+            onChange={(event) => setName(event.target.value)}            
+            value={currentName}
             placeholder="Enter Student Name"
             data-testid="student-name-input"
+    
           />
         <section className="appointment__validation">{error}</section>
         </form>
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={(event) => setInterviewer(event)} />
+        <InterviewerList interviewers={props.interviewers} value={currentInterviewer} onChange={(event) => setInterviewer(event)} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
@@ -57,3 +57,4 @@ export default function Form(props) {
     </main>
   )
 }
+
