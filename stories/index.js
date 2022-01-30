916072ba316e2e8e8d12947e4003df723d9d3c5c
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-01-30 14:27:38 -0500

    Modify stories' index file.

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -6,19 +6,19 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
-import DayListItem from "components/DayListItem"
-import DayList from "components/DayList"
-import InterviewerListItem from "components/InterviewerListItem"
-import InterviewerList from "components/InterviewerList"
-import Appointment from "components/Appointment/index.js"
+import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerList";
+
+import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
-import Show from "components/Appointment/Show"
-import Confirm from "components/Appointment/Confirm"
-import Status from "components/Appointment/Status"
-import Error from "components/Appointment/Error"
-import Form from "components/Appointment/Form"
-
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -35,8 +35,6 @@ storiesOf("Button", module)
       Disabled
     </Button>
   ));
-
-
 
 storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
   .addParameters({
@@ -76,13 +74,7 @@ storiesOf("DayList", module)
   ))
   .add("Tuesday", () => (
     <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
-  ))
-  .add("Wednesday", () => (
-    <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
   ));
-
-
-//Building the Interviewer Item
 
 const interviewer = {
   id: 1,
@@ -114,13 +106,9 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={() => action("setInterviewer")(interviewer.id)}
+      setInterviewer={event => action("setInterviewer")(interviewer.id)}
     />
   ));
-
-
-
-//Building the Interviewer List
 
 const interviewers = [
   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
@@ -137,30 +125,23 @@ storiesOf("InterviewerList", module)
   .add("Initial", () => (
     <InterviewerList
       interviewers={interviewers}
+      setInterviewer={action("setInterviewer")}
     />
   ))
-  .add("Selected", () => (
+  .add("Preselected", () => (
     <InterviewerList
       interviewers={interviewers}
       value={3}
-    />
-  ))
-  .add("Clickable", () => (
-    <InterviewerList
-      interviewers={interviewers}
-      onChange={action("setInterviewer")}
+      onChange={action("onChange")}
     />
   ));
 
-
-// Add appoinment
-
-storiesOf("Appointment", module)
+storiesOf('Appointment', module)
   .addParameters({
-    backgrounds: [{ name: "white", value: "#fff", default: true }]
+    backgrounds: [{ name: 'white', value: '#fff', default: true }]
   })
-  .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => (<Appointment time='12pm' />))
+  .add("Appointment", () => (<Appointment />))
+  .add("Appt with Time", () => (<Appointment time='12pm' />))
   .add("Header", () => (<Header time="12pm" />))
   .add('Empty', () => (<Empty onAdd={action('onAdd')} />))
   .add('Show', () => (
@@ -188,14 +169,14 @@ storiesOf("Appointment", module)
       onClose={action('onClose')}
     />
   ))
-  .add('Create', () => (
+  .add('Form - create', () => (
     <Form
       interviewers={interviewers}
       onSave={action('onSave')}
       onCancel={action('onCancel')}
     />
   ))
-  .add('Edit', () => (
+  .add('Form - edit', () => (
     <Form
       name="Bob"
       interviewers={interviewers}
@@ -206,17 +187,18 @@ storiesOf("Appointment", module)
   ))
   .add("Appointment Empty", () => (
     <Fragment>
-      <Appointment id={1} time="4pm" />
-      <Appointment time="5pm" />
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
     </Fragment>
   ))
   .add("Appointment Booked", () => (
     <Fragment>
       <Appointment
         id={1}
-        time="4pm"
-        interview={{ student: "Lydia Miller-Jones", interviewer }}
+        time="12pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer: interviewer }}
       />
-      <Appointment time="5pm" />
+      <Appointment id="last" time="1pm" />
     </Fragment>
   ))
+
